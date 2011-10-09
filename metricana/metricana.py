55d9d3ddf037ccfe2416137ee39d38dd31dd518e
Thomas Hunger <tehunger@gmail.com>
2011-10-09 13:41:46 +0100

    Import metricana code from test directory.

---
+++ metricana/metricana.py
@@ -0,0 +1,65 @@
+import functools
+import hmac
+import os
+import socket
+import time
+
+class Metricana(object):
+
+    def __init__(self, user_id, api_key, host='metricana.com', port=3000):
+        self.user_id = user_id
+        self.api_key = api_key
+        self.host = host
+        self.port = port
+
+        self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def sign(self, message):
+        full_message = '%d|%d|%s' % (time.time() * 1000, self.user_id, message)
+        return '%s|%s' % (hmac.HMAC(self.api_key, full_message).hexdigest()[:8], full_message)
+
+    def _get_nonce(self):
+        return os.urandom(4).encode('hex')
+
+    def sign(self, message):
+        full_message = '%s|%x|%d|%s' % (self._get_nonce(), time.time(), self.user_id, message)
+        return '%s|%s' % (hmac.HMAC(self.api_key, full_message).hexdigest()[:8], full_message)
+
+    def int(self, key, value):
+        self.send_signed('i|%s|%d' % (key, value))
+
+    def mark(self, key):
+        self.send_signed('m|%s|m' % (key, ))
+
+    def float(self, key, value):
+        self.send_signed('f|%s|%f' % (key, value))
+
+    def send_signed(self, message):
+        print self.sign(message)
+        self.udp_socket.sendto(self.sign(message), socket.MSG_DONTWAIT, (self.host, self.port))
+
+    def runtime(self, key):
+        def create_decorator(f):
+            @functools.wraps(f)
+            def wrapper(*args, **kwargs):
+                t_start = time.time()
+                result = f(*args, **kwargs)
+                t_end = time.time()
+                self.float(key, t_end - t_start)
+                return result
+            return wrapper
+        return create_decorator
+
+    def block_runtime(self, key):
+        class manager:
+            def __init__(self, _key, _outer_self):
+                self.key = _key
+                self._outer_self = _outer_self
+
+            def __enter__(self):
+                self.ts_1 = time.time()
+
+            def __exit__(self, *args):
+                self. _outer_self.float(self.key, time.time() - self.ts_1)
+
+        return manager(key, self)
