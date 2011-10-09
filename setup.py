4573908db4f98bac029791b98dfe435de7df71f2
Thomas Hunger <tehunger@gmail.com>
2011-10-09 16:09:37 +0100

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='metricana',
+    version='0.6',
+    author='Thomas Hunger',
+    author_email='tehunger@gmail.com',
+    packages=['metricana'],
+)
